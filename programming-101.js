79434e9ae467660fc98aa5f73f9dfb97da27c530
saketrajabhishek <saketrajabhishek@gmail.com>
2025-10-04 01:31:20 +0530

    Add initial implementation of basic arithmetic and string concatenation

---
+++ programming-101.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+console.log(8);
+console.log(true);
+
+const a = 10;
+const b = 9;
+
+let firstName = "Saket";
+let lastName = "Abhishek";
+
+const sum = a + b;
+const multiply = a * b;
+const diff = a - b;
+
+console.log(sum, multiply, diff);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
